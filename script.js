93fa04fb6712f28cd2acb2f798fc3c01426430d1
talk2awais <muhammad.awais@southvillesol.com>
2022-06-01 13:39:27 +0500

    added sub directory and onclick event

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// document.getElementsByClassName('abcd').addEventListener("click,abc");
-document.getElementById("abcd").addEventListener("click",abc);
-
-function abc(){
-    let x=document.getElementById("sub-dashboard");
-    // if (x.style.display === "none") {
-    //     x.style.display = "block";
-    //     x.classList.add('active');
-    //   } else {
-    //     x.style.display = "none";
-    //   }
-if(x.classList==="accordian1"){
-    x.classList.remove('accordian1');
-}
-else{
-    x.classList.add("accordian1");
-}
-    
+const accoridan=(e)=>{
+    if (e.children[1].classList[0] === "accordian") {
+        e.children[1].classList.remove('accordian');
+        e.children[1].classList.add('accordian1');
+    }
+    else if (e.children[1].classList[0] === "accordian1") {
+        e.children[1].classList.remove("accordian1");
+        e.children[1].classList.add('accordian');
+    }
+    else {
+        console.log(e.children[1].classList[0]);
+    }
 }
