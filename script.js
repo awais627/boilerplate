a76c78b777ba0825775220529dd81096765689b8
talk2awais <muhammad.awais@southvillesol.com>
2022-06-01 18:12:32 +0500

    onclick Click Collapse

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 const accoridan = (e) => {
     const accordian = e.children[1].classList;
     const angel = e.children[0].children[1].children[0].classList;
+    const accordianAll = document.querySelectorAll(".accordian1");
     if (accordian[0] === "accordian") {
+        accordianAll.forEach(accordianList => {
+            accordianList.classList.remove("accordian1");
+            accordianList.classList.add("accordian");
+        });
         accordian.remove('accordian');
         accordian.add('accordian1');
         angel.remove("rotate-angle-close");
