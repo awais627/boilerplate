68bdcc231371639322b907823add98739f5fad97
talk2awais <muhammad.awais@southvillesol.com>
2022-06-01 18:38:46 +0500

    Update data

--- script.js
+++ script.js
@@ -16,13 +16,10 @@ const accoridan = (e) => {
         angel.remove("rotate-angle-close");
         angel.add("rotate-angle-open");
     }
-    else if (accordian[0] === "accordian1") {
+    else{
         accordian.remove("accordian1");
         accordian.add('accordian');
         angel.remove("rotate-angle-open");
         angel.add("rotate-angle-close");
-    }
-    else {
-        alert("Something went wrong!")
     }
 }
