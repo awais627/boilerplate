2d3bfb4ca0ce9d1846e50e99e3bc397f2fca066b
talk2awais <muhammad.awais@southvillesol.com>
2022-06-01 18:27:05 +0500

    js function changing

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const accoridan = (e) => {
     const accordian = e.children[1].classList;
     const angel = e.children[0].children[1].children[0].classList;
-    const accordianAll = document.querySelectorAll(".accordian1");
+    const accordianAll = document.querySelectorAll(".accordian1")[0];
+    const rotateAngleOpen = document.querySelectorAll(".rotate-angle-open")[0];
     if (accordian[0] === "accordian") {
-        accordianAll.forEach(accordianList => {
-            accordianList.classList.remove("accordian1");
-            accordianList.classList.add("accordian");
-        });
+        if (accordianAll!== undefined) {
+            accordianAll.classList.remove("accordian1");
+            accordianAll.classList.add("accordian");
+            rotateAngleOpen.classList.remove("rotate-angle-open")
+            rotateAngleOpen.classList.add("rotate-angle-close")
+        }
         accordian.remove('accordian');
         accordian.add('accordian1');
         angel.remove("rotate-angle-close");
